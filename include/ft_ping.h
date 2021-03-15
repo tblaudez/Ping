85b5fc6fef643db74a98b4c06a8951d3db8a5d67
Tristan Blaudez <tblaudez@student.42.fr>
2021-03-15 12:15:50 +0000

    Added comment for help

--- include/ft_ping.h
+++ include/ft_ping.h
@@ -6,7 +6,7 @@
 /*   By: tblaudez <tblaudez@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/02/09 14:35:43 by tblaudez      #+#    #+#                 */
-/*   Updated: 2021/03/10 18:02:18 by anonymous     ########   odam.nl         */
+/*   Updated: 2021/03/12 15:53:45 by anonymous     ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,43 +14,20 @@
 # define FT_PING_H
 
 # include <netinet/in.h>
-# include <linux/ip.h>
-# include <linux/icmp.h>
-# include <sys/time.h>
-# include <sys/socket.h>
+# include <netdb.h>
 
-# define PACKET_SIZE 56
+# define M 1000000
+# define DEFAULT_DATALEN 56
 
 
-struct s_send
+extern struct s_ping
 {
-	struct icmphdr		icmp;
-	char				data[PACKET_SIZE];
-};
-
-struct s_receive
-{
-	char				recv_buf[PACKET_SIZE];
-	struct msghdr		msg;
-	struct iovec		iov;
-	struct icmphdr 		recv_icmp;
-	union {
-		char			buff[CMSG_SPACE(sizeof(int))];
-		struct cmsghdr	align;
-	}					control;
-};
-
-struct s_ping
-{
-	struct s_send		send;
-	struct s_receive	receive;
-	struct sockaddr_in	host;
+	struct addrinfo		hostinfo;
 	char				host_ip[INET_ADDRSTRLEN];
+	int					datalen;
 	unsigned char		flags;
 	int					sockfd, ttl;
 	unsigned int		npackets, ntransmitted, nreceived, nlimit, nerror;
-};
-
-extern struct s_ping	g_ping;
+} 						g_ping;
 
 # endif
